13e3c98368a14e61b6057d64121c244e0908fbf9
VininLeao <104996741+VininLeao@users.noreply.github.com>
2024-08-15 22:38:39 -0300

    Update Exercicio12.java

--- Exercicio12.java
+++ Exercicio12.java
@@ -3,6 +3,7 @@ public class Exercicio12 {
         for (int i = 3; i <= 100; i += 3) {
             System.out.println("O valor da divisão de " + i + " por 2 usando int é: " + (i / 2));
             System.out.println("O valor da divisão de " + i + " por 2 usando double é: " + ((double) i / 2));
+            System.out.println();
         }
     }
 }
